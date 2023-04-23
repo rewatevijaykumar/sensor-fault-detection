201a9f44b5dfd1ad9e00f545d559c6c08ccc1d25
rewatevijaykumar <rewatevijaykumar@gmail.com>
2023-04-23 21:55:42 +0530

    custom exception and logger

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+from sensor.logger import logging
+import sys
+
+def test_exception():
+    try:
+        logging.info('Divide number')
+        a = 1/0
+        
+    except Exception as e:
+        raise SensorException(e,sys)
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print("collection name:", mongodb_client.database.list_collection_names())
+    # mongodb_client = MongoDBClient()
+    # print("collection name:", mongodb_client.database.list_collection_names())
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
