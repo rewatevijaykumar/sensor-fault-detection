c807e8023766b839ec828ae262ad4d4c3147d202
rewatevijaykumar <rewatevijaykumar@gmail.com>
2023-04-23 22:52:20 +0530

    define constants

---
+++ sensor/constant/training_pipeline/__init__.py
@@ -0,0 +1,25 @@
+import os
+from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
+
+# define common constant variable for training pipeline
+
+TARGET_COLUMN = 'class'
+PIPELINE_NAME:str = 'sensor'
+ARTIFACT_NAME: str = 'artifact'
+FILE_NAME:str = 'sensor.csv'
+
+TRAIN_FILE_NAME:str = 'train.csv'
+TEST_FILE_NAME:str = 'test.csv'
+
+PREPROCESSING_OBJECT_FILE_NAME:str = 'preprocessing.pkl'
+MODEL_FILE_NAME:str = 'model.pkl'
+SCHEMA_FILE_PATH = os.path.join('config', 'schema.yaml')
+SCHEMA_DROP_COLS = 'drop_columns'
+
+# data ingestion related constant start with DATA_INGESTION VAR NAME
+DATA_INGESTION_COLLECTION_NAME:str = 'car'
+DATA_INGESTION_DIR_NAME:str = 'data_ingestion'
+DATA_INGESTION_FEATURE_STORE:str = 'feature_store'
+DATA_INGESTION_INGESTED_DIR:str = 'ingested'
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
+
